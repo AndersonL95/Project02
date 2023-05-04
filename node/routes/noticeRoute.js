0663e8b87cc1e997ef9ed4fd467151a1e9ac579d
Anderson Alves <anderson@gespag.net.br>
2023-05-04 13:46:32 -0300

    deleteNews_resource

--- node/routes/noticeRoute.js
+++ node/routes/noticeRoute.js
@@ -6,7 +6,7 @@ const auth = require('../utils/auth');
 
 router.post('/create_notice',auth, authAdmin, upload.array("images",5),NewsController.createNews);
 router.get('/noticias',NewsController.getNotice);
-router.get('/noticias/:id', auth, authAdmin, NewsController.deleteNews);
+router.delete('/noticias/:id', auth, authAdmin, NewsController.deleteNews);
 router.put('/noticias/:id', auth, authAdmin, upload.array("images",5),NewsController.updateNews);
 
 module.exports = router
